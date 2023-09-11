cc6d95858b0ef815d71778639cf7a899af5a3937
Akshay Kumar <pawan.akshaykr@gmail.com>
2023-09-11 20:02:13 +0530

    added change

--- src/App/components/registration-form/StyledRegistrationForm.js
+++ src/App/components/registration-form/StyledRegistrationForm.js
@@ -1,9 +1,20 @@
 import styled from "styled-components";
 
 export const StyledRegistrationFormDiv = styled.div`
-  
+  display: block;
 `;
 
-export const StyledTextInput = styled()`
+export const StyledTextInput = styled.input`
+    background-color: honeydew;
+`;
+export const RegistrationFormDivStyled = styled.div`
+  display: block;
+`;
 
+export const NameDivStyled = styled.div`
+    display: flex;
+`;
+
+export const InputStyled = styled.input`
+    background-color: honeydew;
 `;
