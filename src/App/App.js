2641b7cee63ca4412123916f305e570a335a1069
Akshay Kumar <pawan.akshaykr@gmail.com>
2023-09-11 21:03:00 +0530

    added router

--- src/App/App.js
+++ src/App/App.js
@@ -1,12 +1,31 @@
 import { AppDivStyled } from "./StyledApp";
 import RegistrationForm from "./components/registration-form/RegistrationForm";
 import EmployeeList from "./components/employee-list/EmployeeList";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <EmployeeList/>,
+  },
+  {
+    path: "/new",
+    element: <RegistrationForm formMode="new"/>,
+  },
+  {
+    path: "/view/:id",
+    element: <RegistrationForm formMode="view"/>,
+  },
+  {
+    path: "/edit/:id",
+    element: <RegistrationForm formMode="edit"/>,
+  },
+])
 
 function App() {
   return (
     <AppDivStyled>
-      <RegistrationForm />
-      <EmployeeList />
+      <RouterProvider router={routes}/>
     </AppDivStyled>
   );
 }
